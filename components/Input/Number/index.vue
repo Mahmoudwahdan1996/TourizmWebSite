5c538393b4080a02f01e43336bd51187d32468cc
mahmoud <mahmoudsabiha51@gmail.com>
2023-09-11 16:04:36 +0000

    custom-inputs

---
+++ components/Input/Number/index.vue
@@ -0,0 +1,50 @@
+<template>
+  <form-group :name="name" :attribute="name">
+    <template slot-scope="{ attrs, listeners }">
+      <v-label :for="name">
+        <strong class="font-weight-dark">
+          {{ label ? $t(label) : $t(name) }}</strong
+        >
+      </v-label>
+      <v-text-field
+        type="number"
+        :autofocus="focus"
+        :hint="$t(`hint_${name}`)"
+        persistent-hint
+        v-bind="attrs"
+        :id="name"
+        v-model="form[name]"
+        class="mt-1 fill-field"
+        filled
+        rounded
+        flat
+        v-on="listeners"
+      >
+      </v-text-field>
+    </template>
+  </form-group>
+</template>
+
+<script>
+export default {
+  name: "InputWrapperText",
+  props: {
+    form: {
+      type: Object,
+      default: () => {},
+    },
+    name: {
+      type: String,
+      default: () => "",
+    },
+    focus: {
+      type: Boolean,
+      default: () => false,
+    },
+    label: {
+      type: String,
+      default: () => "",
+    },
+  },
+};
+</script>
---
+++ components/Input/Textarea/index.vue
@@ -0,0 +1,56 @@
+<template>
+  <form-group :name="name" :attribute="name">
+    <template slot-scope="{ attrs, listeners }">
+      <v-label :for="name">
+        <strong class="font-weight-dark">
+          {{ label ? $t(label) : $t(name) }}</strong
+        >
+      </v-label>
+      <v-textarea
+        auto-grow
+        variant="filled"
+        color="deep-purple"
+        :rows="rows"
+        :hint="$t(`hint_${name}`)"
+        persistent-hint
+        v-bind="attrs"
+        :id="name"
+        v-model="form[name]"
+        class="mt-1 fill-field"
+        filled
+        rounded
+        flat
+        v-on="listeners"
+      >
+      </v-textarea>
+    </template>
+  </form-group>
+</template>
+
+<script>
+export default {
+  name: "InputTextarea",
+  props: {
+    form: {
+      type: Object,
+      default: () => {},
+    },
+    name: {
+      type: String,
+      default: () => "",
+    },
+    rows: {
+      type: String,
+      default: () => "",
+    },
+    focus: {
+      type: Boolean,
+      default: () => false,
+    },
+    label: {
+      type: String,
+      default: () => null,
+    },
+  },
+};
+</script>
