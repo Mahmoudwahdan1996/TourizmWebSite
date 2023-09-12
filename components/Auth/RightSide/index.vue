ab71f5cb7bebc0f06b8c4c3b426d46afdaf3094a
Mahmoudwahdan1996 <mw01007928381@gmail.com>
2023-09-12 04:20:58 +0300

    signup page

---
+++ components/Auth/RightSide/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-col cols="12" lg="6" class="hidden-md-and-down">
+    <div
+      class="auth__right"
+      :style="`background-image:url(${require(`@/assets/images/auth/${image}`)})`"
+    >
+      <img :src="require(`@/assets/images/auth/air.svg`)" alt="air" />
+    </div>
+  </v-col>
+</template>
+<script>
+export default {
+  name: "RightSide",
+  props: ["image"],
+};
+</script>
--- layouts/auth.vue
+++ layouts/auth.vue
@@ -7,21 +7,21 @@
 <script>
 export default {
   name: "AuthLayout",
-  methods: {
-    setSelectedCurrency() {
-      if (this.sharedCurrencies) {
-        const currency = this.sharedCurrencies.find(
-          (country) => country.code === this.selectedCurrency.code
-        );
-        this.$store.dispatch("currencies/setSelectedCurrency", currency);
-      }
-    },
-    setCurrencies() {
-      this.$store.dispatch("currencies/setCurrencies").then(() => {
-        this.setSelectedCurrency();
-      });
-    },
-  },
+  // methods: {
+  //   setSelectedCurrency() {
+  //     if (this.sharedCurrencies) {
+  //       const currency = this.sharedCurrencies.find(
+  //         (country) => country.code === this.selectedCurrency.code
+  //       );
+  //       this.$store.dispatch("currencies/setSelectedCurrency", currency);
+  //     }
+  //   },
+  //   setCurrencies() {
+  //     this.$store.dispatch("currencies/setCurrencies").then(() => {
+  //       this.setSelectedCurrency();
+  //     });
+  //   },
+  // },
   watch: {
     "$i8n.locale": {
       handler(value) {
@@ -38,7 +38,7 @@ export default {
     "$i18n.locale": {
       handler() {
         this.$store.dispatch("globalSettings/setSettings");
-        this.setCurrencies();
+        // this.setCurrencies();
       },
       immediate: true,
     },
