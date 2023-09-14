7dfe446e519fb7053910cf968c956e6c6b09025f
mahmoudHamdy199 <mahmoud_wahdan@finiexsoft.com>
2023-09-14 18:16:06 +0300

    fix verification

--- pages/verifiication-code/index.vue
+++ pages/verifiication-code/index.vue
@@ -9,5 +9,24 @@ export default {
   name: "VerificationCodePage",
   layout: "auth",
   middleware: "isAuth",
+
+  head() {
+    return {
+      htmlAttrs: {
+        lang: this.$i18n.locale,
+        dir: "ltr",
+      },
+    };
+  },
+
+  watch: {
+    "$i18n.locale": {
+      handler(value) {
+        this.$vuetify.rtl = false;
+        this.isRight = false;
+      },
+      immediate: true,
+    },
+  },
 };
 </script>
